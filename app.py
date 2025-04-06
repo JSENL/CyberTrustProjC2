ab287ba99b2e977a106e2437692a259b0fc7e7fd
Jasen Lambright <jasenlambright@gmail.com>
2025-04-06 18:35:59 -0400

    updated app.py file to have host work, updated connection to connectionS

--- app.py
+++ app.py
@@ -2,22 +2,22 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-app.route('/')
+@app.route('/')
 def welcome():
     return render_template('welcome.html')
 
-app.route('/payloads')
+@app.route('/payloads')
 def payloads():
     return render_template('payloads.html')
 
-app.route('/listeners')
+@app.route('/listeners')
 def listeners():
     return render_template('listeners.html')
 
-app.route('/connection')
+@app.route('/connections')
 def connections():
     return render_template('connection.html')
 
-if __name__ == " __main__":
-    app.run(host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
 
